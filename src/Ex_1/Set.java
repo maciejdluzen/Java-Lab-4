86c68d8d3dc9fc9468dc1003b21d361eedb42d55
Maciej Dluzen <maciej.dluzen@yahoo.com>
2020-01-11 19:18:40 +0000

    Ex 1 in progess

---
+++ src/Ex_1/Set.java
@@ -0,0 +1,27 @@
+package Ex_1;
+
+import java.util.Iterator;
+import java.util.TreeSet;
+
+public class Set<T extends Number> {
+
+    T sum(TreeSet<T> values) {
+
+        values = new TreeSet<>();
+        Iterator setIterator = values.iterator();
+
+        while (setIterator.hasNext()) {
+           setIterator.next();
+
+        }
+
+
+        return
+    }
+
+
+
+
+
+
+}
